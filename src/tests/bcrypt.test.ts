4749c64d68fd69f2eff33b08c7ae17718e6589ca
Pitchakorn-fame <pitchakorn_fame@hotmail.com>
2023-07-26 10:39:57 +0700

    finish test bcrypt pass

--- src/tests/bcrypt.test.ts
+++ src/tests/bcrypt.test.ts
@@ -1,25 +1,20 @@
 import { compareHash, hashPassword } from "../auth/bcrypt"
 
-
 describe('test bcrypt', () => {
     test('compareHash', () => {
-        const hash = hashPassword('123')
-        expect(compareHash('123', hash)).toEqual(true)
+        const hashTests: [string[],boolean][] = [
+            [['123',hashPassword('123')], true],
+            [['abc', hashPassword('abc')], true],
+            [['abc', hashPassword('123')], false],
+            [['567', hashPassword('abc')], false]
+        ];
+
+        hashTests.forEach((test) => {
+            const [input, expected] = test;
+            expect(compareHash(input[0],input[1])).toEqual(expected)
+        }) 
     })
 })
 
 
 
-
-
-
-
-// import { compareHash, hashPassword } from "../auth/bcrypt"
-
-
-// describe('test bcrypt', () => {
-//     test('compareHash 123 and 123', () => {
-//         const hash = hashPassword('123')
-//         expect(compareHash('123',hash)).toBe(true)
-//     })
-// })
